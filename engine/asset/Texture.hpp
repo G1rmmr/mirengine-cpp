48571d2df06803c2dfe43a9ca7c3dc90e40bcc4a
G1rmmr <blckhnd.std@gmail.com>
2026-06-23 15:00:09 +0900

    fix/directories

--- engine/asset/Texture.hpp
+++ engine/asset/Texture.hpp
@@ -4,16 +4,6 @@
 
 using namespace zet;
 
-namespace mir::sound {
+namespace mir::texture {
     void Load(const String<>& name) noexcept;
-
-    void Play(const String<>& name, float volume = 100.f, float pitch = 1.f) noexcept;
-    void PlayAt(const String<>& name, const float x, const float y, float volume = 100.f, float pitch = 1.f) noexcept;
-
-    void PlayBgm(const String<>& name, float volume = 100.f, bool loop = true) noexcept;
-    void StopBgm() noexcept;
-    void SetBgmVolume(float volume) noexcept;
-
-    void SetMasterVolume(float volume) noexcept;
-    void StopAll() noexcept;
 }
--- engine/asset/Video.hpp
+++ engine/asset/Video.hpp
@@ -6,4 +6,9 @@ using namespace zet;
 
 namespace mir::video {
     void Load(const String<>& name) noexcept;
+
+    void Play(const String<>& name, float speed = 1.f) noexcept;
+    void Stop() noexcept;
+    void SetVideoTimeline(const float timeStamp) noexcept;
+    void StopAll() noexcept;
 }
